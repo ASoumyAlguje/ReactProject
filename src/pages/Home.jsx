c5d66fe5f20939322e06bbbfe4dfb7a33bca8eff
ASoumyAlguje <soumyalguje@gmail.com>
2024-12-02 19:32:54 +0530

    Initial project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/navBar'
+import CategoryMenu from '../components/CategoryMenu'
+import FoodItem from '../components/FoodItem'
+const Home = () => {
+  return (
+    <>
+    <NavBar />
+    <CategoryMenu />
+    <FoodItem />
+    </>
+  )
+}
+
+export default Home
